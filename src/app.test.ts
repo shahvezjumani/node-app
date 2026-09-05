717eee91068ac435a940721ebc60966075f78f46
Shahvez <sha685789@gmail.com>
2026-09-05 12:46:52 +0500

    fix issue

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,6 @@
 import it from "node:test";
 import assert from "node:assert";
-import { getCurrentTime } from ".";
+import { getCurrentTime } from "./app";
 
 it("should return the current time", () => {
     const time = getCurrentTime();
